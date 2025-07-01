c03830447264400b670c872ce7efe5538b3c7c4f
Charlie Gallagher <ctgall4@gmail.com>
2025-07-01 17:26:40 -0400

    add animations and redo theme

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,104 @@
+import { Application, Assets, Graphics, Sprite } from "pixi.js";
+
 const templateCard = document.getElementById("project-card");
 const templateRow = document.getElementById("project-row");
 const projects = document.getElementById("projects");
 const prs = document.getElementById("open-source-prs");
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+
+(async () => {
+  const app = new Application();
+  await app.init({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundAlpha: 0,
+    canvas: canvas,
+  });
+
+  Assets.add({
+    alias: "astronaut",
+    src: "/astronaut.png",
+  });
+  Assets.add({
+    alias: "rocket",
+    src: "/rocket.png",
+  });
+
+  const promises = Assets.load(["astronaut", "rocket"]);
+
+  // Load your rocket texture
+  promises.then((textures) => {
+    let astronaut: Sprite | undefined = Sprite.from(textures.astronaut);
+    astronaut.x = 0;
+    astronaut.y = Math.random() * app.screen.height;
+    astronaut.zIndex = 1;
+    astronaut.pivot = (app.screen.width / 2, app.screen.height / 2);
+    astronaut.height = 100;
+    astronaut.width = 100;
+    astronaut.visible = false;
+    let rocket: Sprite | undefined = Sprite.from(textures.rocket);
+    rocket.anchor.set(0.5);
+    rocket.x = app.screen.width - 10; // off-screen left
+    rocket.y = app.screen.height;
+    rocket.width = 100;
+    rocket.height = 100;
+    rocket.rotation = (-1.1 * Math.PI) / 4;
+    rocket.zIndex = 1;
+    let x_vel = 0;
+    let y_vel = 0;
+    app.stage.addChild(rocket);
+    app.stage.addChild(astronaut);
+
+    let count = 0;
+    let totalTime = 0;
+    let time = app.ticker.lastTime;
+
+    const graph = new Graphics()
+      .rect(0, 0, app.screen.width, app.screen.height)
+      .fill({ color: "black" });
+    graph.zIndex = 0;
+
+    app.stage.addChild(graph);
+
+    app.ticker.add((delta) => {
+      const deltaT = delta.lastTime - time;
+      if (rocket) {
+        rocket.rotation -= count / 25000;
+        x_vel -= deltaT / 50000;
+        y_vel -= deltaT / 800 / (count + 1);
+        rocket.x += x_vel;
+        rocket.y += y_vel;
+        graph.y += y_vel;
+        graph.alpha -= 0.001;
+        if (rocket.y < -rocket.height) {
+          rocket.removeFromParent();
+          Assets.unload("rocket");
+          graph.destroy();
+          rocket = undefined;
+        }
+      }
+      if (!rocket && astronaut && totalTime > 10000) {
+        astronaut.visible = true;
+        astronaut.x += 1;
+        astronaut.y += 1;
+        astronaut.rotation += 0.001;
+        if (
+          astronaut.y >= app.screen.height ||
+          astronaut.x >= app.screen.width
+        ) {
+          totalTime = 0;
+          astronaut.visible = false;
+        }
+        astronaut.x %= app.screen.width;
+        astronaut.y %= app.screen.height;
+      }
+      time = delta.deltaMS;
+      totalTime += delta.elapsedMS;
+      count += 1;
+    });
+  });
+})();
 
 const rows = 5;
 const codeExampleNames = [
@@ -60,7 +157,7 @@ for (const [index, article] of articles.entries()) {
   const a = document.createElement("a");
   a.href = "/pages/" + article.file;
   a.innerText = (index + 1).toString() + ") " + article.name;
-  a.className = "mr-auto hover:text-emerald-400 hover:underline";
+  a.className = "mr-auto hover:text-orange-400 hover:underline";
   menu?.appendChild(a);
 }
 
